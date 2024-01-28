e6cbc17a514e644c9b8224ebc07fe6abbfdb1675
Idowu Micheal <idowumicheal2015@gmail.com>
2024-01-28 18:49:39 +0100

    commit

---
+++ home/user/signals.py
@@ -0,0 +1,9 @@
+from .models import Profiles
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender,instance,created,**kwargs):
+    if created:
+        Profiles.objects.create(user=instance)
